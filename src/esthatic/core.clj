fa09f854d06c9e0b3a83d3ae03d1d567e9980172
niquola <niquola@gmail.com>
2016-04-07 16:07:34 +0000

    fix ns name

--- src/esthatic/core.clj
+++ src/esthatic/core.clj
@@ -80,6 +80,16 @@
    :headers {"Content-Type" "text/html; charset=utf-8"}
    :status  200})
 
+(def ^:dynamic *lang* nil)
+(defn url [& parts]
+  (let [root (or (System/getenv "SITE_URL") "/")
+        last (last parts)
+        path (if (map? last) (butlast parts) parts)
+        lang (if *lang* (str (name *lang*) "/") "")
+        params (if (map? last) last {})
+        hash (if (map? last) (:# last) nil)]
+    (str root lang (str/join "/" path) ".html" (when hash (str "#" hash)))))
+
 
 
 (defn mk-dispatch [{routes :routes layout :layout :as opts}]
