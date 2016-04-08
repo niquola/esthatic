d1cebbd53a96ca09d57c0faf35ccf142c36d3d2e
Aitem <muradbei@gmail.com>
2016-04-08 18:21:39 +0300

    fix

--- src/gardner/core.clj
+++ src/gardner/core.clj
@@ -44,7 +44,7 @@
                :right    (when r (u/px* h r))}})))
 
 (defn &c [x]
-  (or (get @colors x) x (throw (Exception. (str "Do not know color " x " only " @colors)))))
+  (or (get @colors x) (throw (Exception. (str "Do not know color " x " only " @colors)))))
 
 (defn &v [x] (and x (u/px* (get @vars :v) x)))
 (defn &h [x] (and x (u/px* (get @vars :h) x)))
@@ -53,7 +53,7 @@
 (defn lighten
   "lighten hex -> hsl ->hex"
   [color amount]
-  (c/hsl->hex (c/lighten  (&c color) amount)))
+  (c/hsl->hex (c/lighten (&c color) amount)))
 
 (defn darken
   "darken hex -> hsl -> hex"
